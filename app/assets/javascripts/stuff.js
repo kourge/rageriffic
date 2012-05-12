069f6ca97870175ab9a30b49ad01535e9a6487ff
Wilson Lee <kourge@gmail.com>
2012-05-12 15:47:10 -0700

    More glue code

--- app/assets/javascripts/stuff.js
+++ app/assets/javascripts/stuff.js
@@ -1,8 +1,39 @@
 
 (function($) {
-var INTERVAL = 5000, ENABLE_POLLING = false;
-var id;
+var INTERVAL = 3000, ENABLE_POLLING = false;
+var id, timer;
 var previousFreeze = false, isFrozen = false;
+
+function loadParticipants() {
+  $.getJSON('/rounds/participations/' + id, function(data) {
+    $(document).trigger('startVoting', data);
+    beginVoting();
+  });
+}
+
+function beginVoting() {
+  timer = window.setInterval(function pollWinner() {
+    $.getJSON('/rounds/voting/' + id, function(data) {
+      if (data.voting_done) {
+        window.clearInterval(timer);
+        $.getJSON('/rounds/winner/' + id, function(data) {
+          $(document).trigger('winnerAnnounced', data);
+        });
+      }
+    });
+  }, INTERVAL);
+}
+
+var params = (function(a) {
+  if (a == "") return {};
+  var b = {};
+  for (var i = 0; i < a.length; ++i) {
+      var p = a[i].split('=');
+      if (p.length != 2) continue;
+      b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+  }
+  return b;
+})(window.location.search.substr(1).split('&'));
 
 $(document).bind('roomFrozen', function() {
   console.log('roomFrozen');
@@ -10,9 +41,15 @@ $(document).bind('roomFrozen', function() {
 
 $(document).bind('startVoting', function() {
   console.log('startVoting');
+  stopPolling();
 });
 
-if (ENABLE_POLLING) window.setInterval(function poll() {
+$(document).bind('imageUploaded', function(event, url) {
+  var iid = url.match(/\/(.{5})\..{3}$/)[1];
+  console.log(iid);
+});
+
+if (ENABLE_POLLING) timer = window.setInterval(function poll() {
   $.getJSON('/rounds/state/' + id, function(data) {
     if (data.is_frozen) {
       isFrozen = true;
@@ -21,7 +58,8 @@ if (ENABLE_POLLING) window.setInterval(function poll() {
       $(document).trigger('roomFrozen');
     }
     if (data.in_room == data.pic_taken) {
-      $(document).trigger('startVoting');
+      window.clearInterval(timer);
+      loadParticipants();
     }
     previousFreeze = isFrozen;
   });
