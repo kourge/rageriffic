0d4407b621a5a337a2495b1aa85e4064e426a7b0
Michael Zhou <mc.zhou@gmail.com>
2012-05-12 14:55:13 -0700

    added trigger to imgur api

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -3,6 +3,7 @@ function UploadToImgur(imagestream) {
          {key: "e0c2bcb83dab53d4d21d7fc77ef3f8c2",
           image: imagestream},
           function(data) {
-           console.log(data.upload.links.original);
+            console.log(data.upload.links.original);
+            $(document).trigger('imageUploaded', data.upload.links.original);
           }, "json");
 }
