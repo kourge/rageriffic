a84ccd758cacc089f60db26b627a83c55982e1fd
Stanley Wang <stanleynwang@gmail.com>
2012-05-12 03:42:10 -0700

    simplified migration

--- db/migrate/20120512095132_create_rounds.rb
+++ db/migrate/20120512095132_create_rounds.rb
@@ -1,21 +1,21 @@
 class CreateRounds < ActiveRecord::Migration
   def self.up
     create_table :rounds do |t|
-      t.string :face, :size => 5, :null => false
-      t.boolean :ended, :null => false, :default => false
-      t.integer :winner, :null => false
+      t.string :face#, :size => 5, :null => false
+      t.boolean :ended#, :null => false, :default => false
+      t.integer :winner#, :null => false
     end
 
-    create_table :participations do
-      t.integer :round_id, :null => false
-      t.boolean :owner, :null => false, :default => false
-      t.string :face, :size => 5, :null => false
-    end
+    #create_table :participations do
+      #t.integer :round_id, :null => false
+      #t.boolean :owner, :null => false, :default => false
+      #t.string :face, :size => 5, :null => false
+    #end
     
   end
 
   def self.down
     drop_table :rounds
-    drop_table :participations
+    #drop_table :participations
   end
 end
