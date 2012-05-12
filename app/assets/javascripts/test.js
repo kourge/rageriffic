df6f7e46d85f0cb9257be41c5063a522672afde7
Michael Zhou <mc.zhou@gmail.com>
2012-05-12 14:10:11 -0700

    updated imgur api

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -1,10 +1,9 @@
 function Upload(imagestream) {
   $.post("http://api.imgur.com/2/upload.json",
          {key: "e0c2bcb83dab53d4d21d7fc77ef3f8c2",
-          image: "http://static.jquery.com/files/rocker/images/logo_jquery_215x53.gif"},
+          image: imagestream},
           function(data) {
-           
-           return data.upload.links.original;
+           console.log(data.upload.links.original);
           }, "json");
 }
 
