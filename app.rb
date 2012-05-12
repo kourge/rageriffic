e687aa54489bd49faa34f764ce754d145ea9fabc
Stanley Wang <stanleynwang@gmail.com>
2012-05-11 22:01:15 -0700

    Set root dir

--- app.rb
+++ app.rb
@@ -3,13 +3,15 @@ require 'mustache/sinatra'
 require 'sequel'
 
 class Rageriffic < Sinatra::Application
-  require 'views/layout'
   enable :sessions
 
+  set :root, File.dirname(__FILE__)
   set :mustache, {
     :views     => File.expand_path('views/'),
     :templates => File.expand_path('templates/')
   }
+
+  require 'views/layout'
 
   get '/' do
     #"Hello, World!"
