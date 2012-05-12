ccea6f23d70fca1f39a5e92a3fdb65deccf11d8c
Stanley Wang <stanleynwang@gmail.com>
2012-05-12 14:43:11 -0700

    Set default value of is_frozen for Round to fale

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,4 +1,11 @@
 class Round < ActiveRecord::Base
   has_many :participations
   has_many :votes
+  before_save :force_unfrozen
+
+  def force_unfrozen
+    self.is_frozen = false
+    true
+  end
+
 end
